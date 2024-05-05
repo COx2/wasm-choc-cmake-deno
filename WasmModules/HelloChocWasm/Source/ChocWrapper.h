44a3f50f14df7475cdbc458d15c6b8886dd40e78
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-05-05 09:43:34 +0900

    Cleanup

--- WasmModules/HelloChocWasm/Source/ChocWrapper.h
+++
@@ -1,4 +0,0 @@
-extern "C" 
-{
-    // const char* getPlatformName();
-}
--- WasmModules/HelloChocWasm/Source/Main.cpp
+++ WasmModules/HelloChocWasm/Source/Main.cpp
@@ -1,10 +1,10 @@
 #include <platform/choc_Platform.h>
-// #include <iostream>
+#include <iostream>
 
-// int main (int argc, char* argv[])
-// {
-//     // Your code goes here!
-//     std::cout << "Hello with CHOC, this platform is: " << CHOC_OPERATING_SYSTEM_NAME << std::endl;
+int main (int argc, char* argv[])
+{
+    // Your code goes here!
+    std::cout << "Hello with CHOC, this platform is: " << CHOC_OPERATING_SYSTEM_NAME << std::endl;
 
-//     return 0;
-// }
+    return 0;
+}
